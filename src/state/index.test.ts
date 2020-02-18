1996bb190332d0c202a5ab056932ce0e61517b5b
Yuxi Wang <yuxi.wangcd@gmail.com>
2020-02-17 21:55:17 -0700

    Test state.update and reset

--- src/state/index.test.ts
+++ src/state/index.test.ts
@@ -6,4 +6,23 @@ describe('createStateControll', () => {
     const initialState = { initial: 'state' }
     assert.deepEqual(createStateControll(initialState).getState(), initialState)
   })
+
+  it('should update state by passing a new state', () => {
+    const update = { initial: 'changed' }
+    const stateControll = createStateControll({ initial: 'state' })
+
+    stateControll.update(update)
+
+    assert.deepEqual(stateControll.getState(), update)
+  })
+
+  it('should reset to initial state', () => {
+    const initial = { initial: 'state' }
+    const stateControll = createStateControll({ initial: 'state' })
+    stateControll.update({ initial: 'changed' })
+
+    stateControll.reset()
+
+    assert.deepEqual(stateControll.getState(), initial)
+  })
 })
