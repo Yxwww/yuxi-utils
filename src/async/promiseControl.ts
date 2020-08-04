6e1908b584ef1aeb42f491d28fbcc30ee0b791a6
Yuxi Wang <yuxi.wangcd@gmail.com>
2020-08-04 09:58:44 -0600

    Add createPromiseControl

---
+++ src/async/promiseControl.ts
@@ -0,0 +1,24 @@
+type Subscriber<T> = (arg: T) => void
+type Unsubscriber = () => void
+
+export function createPromiseControl<T>(promise: Promise<T>) {
+  const subscribers: Subscriber<T>[] = []
+  return {
+    subscribe(subscriber: Subscriber<T>): Unsubscriber {
+      subscribers.push(subscriber)
+      promise.then(v => {
+        subscribers.forEach(fn => fn(v))
+      })
+      return () => {
+        const index = subscribers.indexOf(subscriber)
+        subscribers.splice(index, 1)
+      }
+    },
+    dispose() {
+      subscribers.length = 0
+    },
+    getPromise() {
+      return promise
+    },
+  }
+}
